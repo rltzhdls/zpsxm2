5ae6922774df3d585f5d8a35c921ab8f0d1bce7d
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-10-30 16:00:06 +0900

    Add files via upload

---
+++ udks4af.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://cdn.jsdelivr.net/gh/rltzhdls/dpeps3@main/Lineage.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
